6fd7e5b3fcf6041316c3487e9a25dee249a70793
lunartide <lunartide@users.noreply.github.com>
2016-10-15 21:52:55 -0400

    Bug fixes, added mirrors, improvements

--- screenshot.sh
+++ screenshot.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-retries=1 # number of times to retry (per host) if the upload fails
+# upload mirrors
+host_upload_urls[0]='https://desu.sh/upload.php' # the url of the pomf upload script
+host_content_urls[0]='https://a.desu.sh' # the url where images are stored on the host
 
-host_upload_urls[0]='http://pomf.cat/upload.php' # the url of the pomf upload script
-host_content_urls[0]='http://a.pomf.cat' # the url where images are stored on the host
+host_upload_urls[1]='https://pomfe.co/upload.php'
+host_content_urls[1]='https://a.pomfe.co'
+
+host_upload_urls[2]='http://pomf.cat/upload.php'
+host_content_urls[2]='http://a.pomf.cat'
 
 echo "drag to select area"
 
@@ -17,31 +22,27 @@ fi
 echo "uploading..."
 
 i=0
-while [[ $i -le ${#host_upload_urls[@]} ]]; do
+while [[ $i -lt ${#host_upload_urls[@]} ]]; do
   host_upload_url=${host_upload_urls[i]}
   host_content_url=${host_content_urls[i]}
 
-  try=0
-  while [[ $try -le $retries ]]; do
-    curl_response=$(curl --silent -sf -F files[]="@/tmp/screenshot.png" "${host_upload_url}")
-  	if [[ "${curl_response}" =~ '"success":true,' ]]; then
-  		return_file=$(echo "$curl_response" | grep -Eo '"url":"[A-Za-z0-9]+.png",' | sed 's/"url":"//;s/",//')
-  		break
-  	else
-      echo "upload to ${host_upload_url} failed, retrying"
-  		((n = n+1))
-  	fi
-  done
-
-  if [[ -n ${return_file} ]]; then
-    echo "upload complete: ${host_content_url}/${return_file}"
-    xmessage -nearmouse -buttons "" -timeout 2 "upload complete"
-    echo -n "${host_content_url}/${return_file}" | xclip -selection clipboard
-    exit 0
-  else
-    echo "upload failed"
-    xmessage -nearmouse -buttons "" -timeout 3 "upload failed"
-  fi
+  curl_response=$(curl --silent -sf -F files[]="@/tmp/screenshot.png" "${host_upload_url}")
+	if [[ "${curl_response}" =~ '"success":true,' ]]; then
+		return_file=$(echo "$curl_response" | grep -Eo '"url":"[A-Za-z0-9]+.png",' | sed 's/"url":"//;s/",//')
+		break
+	else
+    echo "upload to ${host_upload_url} failed"
+	fi
 
   ((i = i+1))
 done
+
+if [[ -n ${return_file} ]]; then
+  echo "upload complete: ${host_content_url}/${return_file}"
+  xmessage -nearmouse -buttons "" -timeout 2 "upload complete" 2> /dev/null
+  echo -n "${host_content_url}/${return_file}" | xclip -selection clipboard
+  exit 0
+else
+  echo "upload failed"
+  xmessage -nearmouse -buttons "" -timeout 3 "upload failed" 2> /dev/null
+fi
